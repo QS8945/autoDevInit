6e3146e920ea1381e19ade9baf315f49d4acf2f5
QS8945 <1119225756@qq.com>
2017-08-04 05:30:41 +0800

    发布

---
+++ scripts/category.js
@@ -0,0 +1,1 @@
+!function(n,t){t.querySelectorAll(".content-left>ul>li").forEach(function(n){n.addEventListener("click",function(){t.querySelectorAll(".content-left>ul>li").forEach(function(n){n.classList.remove("now")}),this.classList.add("now")})})}(window,document);
